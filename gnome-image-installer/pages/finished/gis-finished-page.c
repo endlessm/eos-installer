5a137eeb8214740abad726b73f55bf648ab5bbb3
Kalle Vahlman <kalle.vahlman@movial.com>
2016-05-18 19:21:00 +0300

    Enable poweroff in final page

--- gnome-image-installer/pages/finished/gis-finished-page.c
+++ gnome-image-installer/pages/finished/gis-finished-page.c
@@ -51,6 +51,7 @@ G_DEFINE_TYPE_WITH_PRIVATE (GisFinishedPage, gis_finished_page, GIS_TYPE_PAGE);
 static void
 reboot_cb (GtkButton *button, GisFinishedPage *page)
 {
+  g_spawn_command_line_sync ("/usr/bin/systemctl poweroff", NULL, NULL, NULL, NULL);
   g_application_quit(G_APPLICATION (GIS_PAGE (page)->driver));
 }
 
