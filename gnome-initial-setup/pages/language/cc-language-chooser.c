6115611f274e04f2a5e7d732479b8c887efd2920
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2016-06-08 00:03:22 -0300

    language-chooser: normalize, unaccent and casefold compared strings
    To further improve the sorting of the language chooser, lower cased,
    accented and non-casefolded strings should be compared equally.
    
    This patch makes the sorting function insensitive to these factors.
    
    https://phabricator.endlessm.com/T11916

--- gnome-initial-setup/pages/language/cc-language-chooser.c
+++ gnome-initial-setup/pages/language/cc-language-chooser.c
@@ -25,6 +25,7 @@
 #include "config.h"
 #include "cc-language-chooser.h"
 
+#include <string.h>
 #include <locale.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
@@ -307,12 +308,90 @@ language_visible (GtkListBoxRow *row,
         return visible;
 }
 
+/*
+ * Copied from tracker/src/libtracker-fts/tracker-parser-glib.c under the GPL
+ * And then from gnome-shell/src/shell-util.c
+ *
+ * Originally written by Aleksander Morgado <aleksander@gnu.org>
+ */
+#define IS_SOFT_HYPHEN(c) ((c) == 0x00AD)
+#define IS_CDM_UCS4(c) (((c) >= 0x0300 && (c) <= 0x036F)  || \
+                        ((c) >= 0x1DC0 && (c) <= 0x1DFF)  || \
+                        ((c) >= 0x20D0 && (c) <= 0x20FF)  || \
+                        ((c) >= 0xFE20 && (c) <= 0xFE2F))
+
+static gchar*
+normalize_casefold_and_unaccent (const gchar *str)
+{
+  gchar *normalized, *tmp;
+  gint i = 0, j = 0, ilen;
+
+  if (str == NULL)
+    return NULL;
+
+  normalized = g_utf8_normalize (str, -1, G_NORMALIZE_NFKD);
+  tmp = g_utf8_casefold (normalized, -1);
+  g_free (normalized);
+
+  ilen = strlen (tmp);
+
+  while (i < ilen)
+    {
+      gunichar unichar;
+      gchar *next_utf8;
+      gint utf8_len;
+
+      /* Get next character of the word as UCS4 */
+      unichar = g_utf8_get_char_validated (&tmp[i], -1);
+
+      /* Invalid UTF-8 character or end of original string. */
+      if (unichar == (gunichar) -1 ||
+          unichar == (gunichar) -2)
+        {
+          break;
+        }
+
+      /* Find next UTF-8 character */
+      next_utf8 = g_utf8_next_char (&tmp[i]);
+      utf8_len = next_utf8 - &tmp[i];
+
+      if (IS_CDM_UCS4 (unichar) || IS_SOFT_HYPHEN (unichar))
+        {
+          /* If the given unichar is a combining diacritical mark,
+           * just update the original index, not the output one */
+          i += utf8_len;
+          continue;
+        }
+
+      /* If already found a previous combining
+       * diacritical mark, indexes are different so
+       * need to copy characters. As output and input
+       * buffers may overlap, need to use memmove
+       * instead of memcpy */
+      if (i != j)
+        {
+          memmove (&tmp[j], &tmp[i], utf8_len);
+        }
+
+      /* Update both indexes */
+      i += utf8_len;
+      j += utf8_len;
+    }
+
+  /* Force proper string end */
+  tmp[j] = '\0';
+
+  return tmp;
+}
+
 static gint
 sort_languages (GtkListBoxRow *a,
                 GtkListBoxRow *b,
                 gpointer       data)
 {
         LanguageWidget *la, *lb;
+        gchar *normalized_a, *normalized_b;
+        gint retval;
 
         la = get_language_widget (gtk_bin_get_child (GTK_BIN (a)));
         lb = get_language_widget (gtk_bin_get_child (GTK_BIN (b)));
@@ -323,7 +402,15 @@ sort_languages (GtkListBoxRow *a,
         if (lb == NULL)
                 return -1;
 
-        return strcmp (la->locale_name, lb->locale_name);
+        normalized_a = normalize_casefold_and_unaccent (la->locale_name);
+        normalized_b = normalize_casefold_and_unaccent (lb->locale_name);
+
+        retval = strcmp (normalized_a, normalized_b);
+
+        g_free (normalized_a);
+        g_free (normalized_b);
+
+        return retval;
 }
 
 static void
