endlessm/eos-installer

commit 9fc5a804dcc98505375b4d199499fe3b6ce1e284

finished: adjust success label for DVD vs USB

gnome-image-installer/pages/finished/gis-finished-page.c
@@ -38,6 +38,8 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#include <udisks/udisks.h>
+
 struct _GisFinishedPagePrivate {
   gint led_state;
 };
@@ -88,6 +90,14 @@ gis_finished_page_shown (GisPage *page)
         {
           g_timeout_add_seconds (1, (GSourceFunc)toggle_leds, page);
         }
+    }
+  else
+    {
+      UDisksDrive *drive = UDISKS_DRIVE (gis_store_get_object (GIS_STORE_IMAGE_DRIVE));
+      gboolean optical = udisks_drive_get_optical (drive);
+
+      gtk_widget_set_visible (WID ("removelabel_usb"), !optical);
+      gtk_widget_set_visible (WID ("removelabel_dvd"),  optical);
     }
 }
 

gnome-image-installer/pages/finished/gis-finished-page.ui
@@ -31,11 +31,30 @@
               </packing>
             </child>
             <child>
-              <object class="GtkLabel" id="removelabel">
+              <object class="GtkLabel" id="removelabel_usb">
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
                 <property name="margin_top">16</property>
                 <property name="label" translatable="yes">Success! Power off your computer and then remove your USB device. When you restart, Endless OS will be ready for you to set up and enjoy!</property>
+                <property name="justify">center</property>
+                <property name="wrap">True</property>
+                <property name="max_width_chars">50</property>
+                <attributes>
+                  <attribute name="weight" value="bold"/>
+                </attributes>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+            <child>
+              <object class="GtkLabel" id="removelabel_dvd">
+                <property name="visible">True</property>
+                <property name="can_focus">False</property>
+                <property name="margin_top">16</property>
+                <property name="label" translatable="yes">Success! Power off your computer and remove the DVD. When you restart, Endless OS will be ready for you to set up and enjoy!</property>
                 <property name="justify">center</property>
                 <property name="wrap">True</property>
                 <property name="max_width_chars">50</property>

commit 655046cffa7fe899cbe3621f36ceafe39354cc44

finished: don't crash when running from exFAT
9fc5a80 introduced a crash in the case where
`gis_store_get_object (GIS_STORE_IMAGE_DRIVE)` returns `NULL`. As
explained in the previous commit, this is the case when booting from an
exFAT- or NTFS-based combined USB (as opposed to an ISO, where I was
testing this).

https://phabricator.endlessm.com/T19078

gnome-image-installer/pages/finished/gis-finished-page.c
@@ -93,8 +93,12 @@ gis_finished_page_shown (GisPage *page)
     }
   else
     {
-      UDisksDrive *drive = UDISKS_DRIVE (gis_store_get_object (GIS_STORE_IMAGE_DRIVE));
-      gboolean optical = udisks_drive_get_optical (drive);
+      /* When the image is on an exFAT partition, GIS_STORE_IMAGE_DRIVE will be
+       * NULL. In this case, we can safely assume it's not optical!
+       */
+      GObject *drive = gis_store_get_object (GIS_STORE_IMAGE_DRIVE);
+      gboolean optical = drive != NULL &&
+        udisks_drive_get_optical (UDISKS_DRIVE (drive));
 
       gtk_widget_set_visible (WID ("removelabel_usb"), !optical);
       gtk_widget_set_visible (WID ("removelabel_dvd"),  optical);