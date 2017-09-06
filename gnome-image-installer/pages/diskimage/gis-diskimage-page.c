endlessm/eos-installer

commit fcdf1f927ec5275b664bd13d9b64570309ae5382

Store UDisksDrive, not its path
This will make it easier to pull out other properties as needed, namely the
'optical' property on the finish page.

gnome-image-installer/pages/diskimage/gis-diskimage-page.c
@@ -617,6 +617,7 @@ gis_diskimage_page_mount (GisPage *page)
       UDisksFilesystem *fs = NULL;
       const gchar *const*mounts = NULL;
       const gchar *dev = NULL;
+      UDisksDrive *drive = NULL;
 
       if (block == NULL)
         continue;
@@ -654,7 +655,9 @@ gis_diskimage_page_mount (GisPage *page)
                                         (GAsyncReadyCallback)gis_diskimage_page_mount_ready, page);
         }
 
-      gis_store_set_image_drive (udisks_block_get_drive (block));
+      drive = udisks_client_get_drive_for_block (client, block);
+      gis_store_set_object (GIS_STORE_IMAGE_DRIVE, G_OBJECT (drive));
+      g_clear_object (&drive);
       return;
     }
 

gnome-image-installer/pages/disktarget/gis-disktarget-page.c
@@ -267,7 +267,11 @@ gis_disktarget_page_populate_model(GisPage *page, UDisksClient *client)
   GtkTreeIter i;
   gchar *umodel = NULL;
   UDisksDrive *root = NULL;
-  const gchar *image_drive = NULL;
+  UDisksDrive *image_drive = UDISKS_DRIVE (gis_store_get_object (GIS_STORE_IMAGE_DRIVE));
+  const gchar *image_drive_path = NULL;
+
+  if (image_drive != NULL)
+    image_drive_path = g_dbus_proxy_get_object_path (G_DBUS_PROXY (image_drive));
 
   if (gis_store_is_unattended())
     {
@@ -281,7 +285,6 @@ gis_disktarget_page_populate_model(GisPage *page, UDisksClient *client)
   priv->has_valid_disks = FALSE;
   gtk_list_store_clear(store);
   root = gis_disktarget_page_get_root_drive (client);
-  image_drive = gis_store_get_image_drive ();
   for (l = objects; l != NULL; l = l->next)
     {
       gchar *targetname, *targetsize;
@@ -318,7 +321,7 @@ gis_disktarget_page_populate_model(GisPage *page, UDisksClient *client)
       block = udisks_client_get_block_for_drive(client, drive, TRUE);
       skip_if (block == NULL, "no corresponding block object");
 
-      skip_if (0 == g_strcmp0 (udisks_block_get_drive (block), image_drive),
+      skip_if (0 == g_strcmp0 (object_path, image_drive_path),
                "it hosts the image partition");
 #undef skip_if
 

gnome-image-installer/util/gis-store.c
@@ -30,7 +30,6 @@ static GObject *_objects[GIS_STORE_N_OBJECTS];
 static guint64 _size = 0;
 static gint64 _image_size = 0;
 static gchar *_name = NULL;
-static gchar *_drive = NULL;
 static gchar *_signature = NULL;
 static GError *_error = NULL;
 static gboolean _unattended = FALSE;
@@ -98,17 +97,6 @@ void gis_store_clear_image_name(void)
 {
   g_free (_name);
   _name = NULL;
-}
-
-const gchar *gis_store_get_image_drive(void)
-{
-  return _drive;
-}
-
-void gis_store_set_image_drive(const gchar *drive)
-{
-  g_free (_drive);
-  _drive = g_strdup (drive);
 }
 
 const gchar *gis_store_get_image_signature (void)

gnome-image-installer/util/gis-store.h
@@ -31,9 +31,18 @@
 G_BEGIN_DECLS
 
 typedef enum {
+  /* GFile: selected image path */
   GIS_STORE_IMAGE = 0,
+
+  /* UDisksBlock: block device to reformat */
   GIS_STORE_BLOCK_DEVICE,
+
+  /* UDisksClient: global shared UDisks client proxy */
   GIS_STORE_UDISKS_CLIENT,
+
+  /* UDisksDrive: drive hosting partition hosting GIS_STORE_IMAGE */
+  GIS_STORE_IMAGE_DRIVE,
+
   GIS_STORE_N_OBJECTS
 } GISStoreObjectKey;
 
@@ -50,9 +59,6 @@ void gis_store_set_image_size (gint64 size);
 gchar *gis_store_get_image_name(void);
 void gis_store_set_image_name(gchar *name);
 void gis_store_clear_image_name(void);
-
-const gchar *gis_store_get_image_drive(void);
-void gis_store_set_image_drive(const gchar *drive);
 
 const gchar *gis_store_get_image_uuid(void);
 void gis_store_set_image_uuid(const gchar *uuid);

commit 29f3d690519d83b2685dc15b7576357b07cdd951

diskimage: handle NULL image drive
This is a regression in fcdf1f9. G_OBJECT (NULL) triggers a CRITICAL:

    GLib-GObject-CRITICAL **: g_object_ref: assertion 'G_IS_OBJECT (object)' failed

gnome-image-installer/pages/diskimage/gis-diskimage-page.c
@@ -656,8 +656,17 @@ gis_diskimage_page_mount (GisPage *page)
         }
 
       drive = udisks_client_get_drive_for_block (client, block);
-      gis_store_set_object (GIS_STORE_IMAGE_DRIVE, G_OBJECT (drive));
-      g_clear_object (&drive);
+      if (drive != NULL)
+        {
+          gis_store_set_object (GIS_STORE_IMAGE_DRIVE, G_OBJECT (drive));
+          g_clear_object (&drive);
+        }
+      /* If running from exFAT or NTFS, where we use device mapper rather than
+       * loopback mount, the image host partition we use is on another mapped
+       * device (because we can't mount the real one directly). In this case,
+       * the UDisksBlock has no associated UDisksDrive.
+       */
+
       return;
     }
 