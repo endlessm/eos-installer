01a0889080762182b81fab1abd6e0a57ab341ddf
Will Thompson <will@willthompson.co.uk>
2018-01-18 17:01:18 +0000

    unattended: introduce GisUnattendedConfig stub
    At present, the keyfile is parsed up-front into a GKeyFile, which is
    stashed in the store, and each page plucks keys out of it. This works,
    but it makes it harder to see what information is retrieved from the
    file (since it's spread out through the program) and means the keyfile
    cannot be validated up-front.
    
    We will gradually move this into this class, improving and extending the
    file format as we go.
    
    https://phabricator.endlessm.com/T14343

---
+++ gnome-image-installer/util/gis-unattended-config.c
@@ -0,0 +1,172 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright © 2018 Endless Mobile, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+#include "config.h"
+#include "gis-unattended-config.h"
+
+#include <glib/gi18n.h>
+
+#include "glnx-errors.h"
+
+typedef struct _GisUnattendedConfig {
+  GObject parent;
+
+  gchar *file_path;
+  gboolean initialized;
+
+  GKeyFile *key_file;
+} GisUnattendedConfig;
+
+G_DEFINE_QUARK (gis-unattended-error, gis_unattended_error);
+
+static void gis_unattended_config_initable_iface_init (GInitableIface *iface);
+
+G_DEFINE_TYPE_EXTENDED (GisUnattendedConfig,
+                        gis_unattended_config,
+                        G_TYPE_OBJECT,
+                        0,
+                        G_IMPLEMENT_INTERFACE (G_TYPE_INITABLE,
+                                               gis_unattended_config_initable_iface_init));
+
+typedef enum {
+  PROP_FILE_PATH = 1,
+  N_PROPERTIES
+} GisUnattendedConfigPropertyId;
+
+static GParamSpec *props[N_PROPERTIES] = { 0 };
+
+static void
+gis_unattended_config_init (GisUnattendedConfig *self)
+{
+  self->key_file = g_key_file_new ();
+}
+
+static void
+gis_unattended_config_set_property (GObject      *object,
+                                    guint         property_id,
+                                    const GValue *value,
+                                    GParamSpec   *pspec)
+{
+  GisUnattendedConfig *self = GIS_UNATTENDED_CONFIG (object);
+
+  switch ((GisUnattendedConfigPropertyId) property_id)
+    {
+    case PROP_FILE_PATH:
+      g_free (self->file_path);
+      self->file_path = g_value_dup_string (value);
+      break;
+
+    case N_PROPERTIES:
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+    }
+}
+
+static void
+gis_unattended_config_dispose (GObject *object)
+{
+  GisUnattendedConfig *self = GIS_UNATTENDED_CONFIG (object);
+
+  g_clear_pointer (&self->key_file, g_key_file_unref);
+
+  G_OBJECT_CLASS (gis_unattended_config_parent_class)->dispose (object);
+}
+
+static void
+gis_unattended_config_finalize (GObject *object)
+{
+  GisUnattendedConfig *self = GIS_UNATTENDED_CONFIG (object);
+
+  g_clear_pointer (&self->file_path, g_free);
+
+  G_OBJECT_CLASS (gis_unattended_config_parent_class)->finalize (object);
+}
+
+static void
+gis_unattended_config_class_init (GisUnattendedConfigClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->set_property = gis_unattended_config_set_property;
+  object_class->dispose = gis_unattended_config_dispose;
+  object_class->finalize = gis_unattended_config_finalize;
+
+  props[PROP_FILE_PATH] = g_param_spec_string (
+      "file-path",
+      "file path",
+      "Path to unattended.ini file",
+      NULL,
+      G_PARAM_WRITABLE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, N_PROPERTIES, props);
+}
+
+static gboolean
+gis_unattended_config_initable_init (GInitable    *initable,
+                                     GCancellable *cancellable,
+                                     GError      **error)
+{
+  GisUnattendedConfig *self = GIS_UNATTENDED_CONFIG (initable);
+  g_autoptr(GError) local_error = NULL;
+
+  g_return_val_if_fail (self->file_path != NULL, FALSE);
+
+  /* This class does not support being initialized multiple times. */
+  g_return_val_if_fail (!self->initialized, FALSE);
+  self->initialized = TRUE;
+
+  if (!g_key_file_load_from_file (self->key_file,
+                                  self->file_path,
+                                  G_KEY_FILE_NONE,
+                                  &local_error))
+    {
+      if (!g_error_matches (local_error, G_FILE_ERROR, G_FILE_ERROR_NOENT))
+        {
+          /* Force all other errors into the GIS_UNATTENDED_ERROR domain,
+           * but leave ENOENT untouched so it can be easily caught.
+           */
+          local_error->domain = GIS_UNATTENDED_ERROR;
+          local_error->code = GIS_UNATTENDED_ERROR_READ;
+        }
+
+      g_propagate_error (error, g_steal_pointer (&local_error));
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
+static void
+gis_unattended_config_initable_iface_init (GInitableIface *iface)
+{
+  iface->init = gis_unattended_config_initable_init;
+}
+
+GisUnattendedConfig *
+gis_unattended_config_new (const gchar *file_path,
+                           GError **error)
+{
+  g_return_val_if_fail (file_path != NULL, NULL);
+  g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+
+  return g_initable_new (GIS_TYPE_UNATTENDED_CONFIG, NULL, error,
+                         "file-path", file_path,
+                         NULL);
+}
---
+++ tests/test-unattended-config.c
@@ -0,0 +1,164 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright © 2018 Endless Mobile, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+#include "config.h"
+#include <locale.h>
+#include <string.h>
+#include <glib/gstdio.h>
+
+#include "gis-unattended-config.h"
+
+typedef struct {
+    gchar *tmpdir;
+} Fixture;
+
+static void
+fixture_set_up (Fixture *fixture,
+                gconstpointer user_data)
+{
+  g_autoptr(GError) error = NULL;
+
+  fixture->tmpdir = g_dir_make_tmp ("eos-installer.XXXXXX", &error);
+  g_assert_no_error (error);
+  g_assert_nonnull (fixture->tmpdir);
+}
+
+static void
+rm_r (const gchar *path)
+{
+  g_autoptr(GFile) file = g_file_new_for_path (path);
+  g_autoptr(GFileEnumerator) enumerator = NULL;
+  GFile *child = NULL;
+  GError *error = NULL;
+
+  enumerator = g_file_enumerate_children (
+      file, NULL, G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS, NULL, &error);
+  g_assert_no_error (error);
+
+  while (g_file_enumerator_iterate (enumerator, NULL, &child, NULL, &error) &&
+         child != NULL)
+    {
+      if (!g_file_delete (child, NULL, &error))
+        {
+          g_autofree gchar *child_path = g_file_get_path (child);
+          g_warning ("Failed to delete %s: %s", child_path, error->message);
+          g_clear_error (&error);
+        }
+    }
+
+  if (error != NULL)
+    {
+      g_warning ("Error while enumerating %s: %s", path, error->message);
+      g_clear_error (&error);
+    }
+
+  if (!g_file_delete (file, NULL, &error))
+    {
+      g_warning ("Failed to delete %s: %s", path, error->message);
+      g_clear_error (&error);
+    }
+}
+
+static void
+fixture_tear_down (Fixture *fixture,
+                   gconstpointer user_data)
+{
+  rm_r (fixture->tmpdir);
+  g_clear_pointer (&fixture->tmpdir, g_free);
+}
+
+static void
+test_parse_empty (void)
+{
+  g_autofree gchar *empty_ini =
+    g_test_build_filename (G_TEST_DIST, "unattended/empty.ini", NULL);
+  g_autoptr(GisUnattendedConfig) config = NULL;
+  g_autoptr(GError) error = NULL;
+
+  config = gis_unattended_config_new (empty_ini, &error);
+  g_assert_no_error (error);
+  g_assert_nonnull (config);
+}
+
+static void
+test_parse_malformed (void)
+{
+  /* This source file is a perfectly good non-keyfile! */
+  g_autofree gchar *not_ini =
+    g_test_build_filename (G_TEST_DIST, "test-unattended-config.c", NULL);
+  g_autoptr(GisUnattendedConfig) config = NULL;
+  g_autoptr(GError) error = NULL;
+
+  config = gis_unattended_config_new (not_ini, &error);
+  g_assert_error (error,
+                  GIS_UNATTENDED_ERROR,
+                  GIS_UNATTENDED_ERROR_READ);
+  g_assert_null (config);
+}
+
+static void
+test_parse_noent (void)
+{
+  g_autofree gchar *ini =
+    g_test_build_filename (G_TEST_DIST, "does-not-exist", NULL);
+  g_autoptr(GisUnattendedConfig) config = NULL;
+  g_autoptr(GError) error = NULL;
+
+  config = gis_unattended_config_new (ini, &error);
+  /* We wrap most errors in our own GIS_UNATTENDED_ERROR domain to simplify
+   * error reporting on the final page, but leave NOENT untouched since it is
+   * not an error for the application as a whole.
+   */
+  g_assert_error (error,
+                  G_FILE_ERROR,
+                  G_FILE_ERROR_NOENT);
+  g_assert_null (config);
+}
+
+static void
+test_parse_unreadable (Fixture *fixture,
+                       gconstpointer data)
+{
+  g_autoptr(GisUnattendedConfig) config = NULL;
+  g_autoptr(GError) error = NULL;
+
+  /* The test tmpdir is just a convenient path that exists, but isn't a file */
+  config = gis_unattended_config_new (fixture->tmpdir, &error);
+  g_assert_error (error,
+                  GIS_UNATTENDED_ERROR,
+                  GIS_UNATTENDED_ERROR_READ);
+  g_assert_null (config);
+}
+
+int
+main (int argc, char *argv[])
+{
+  setlocale (LC_ALL, "");
+
+  g_test_init (&argc, &argv, NULL);
+  g_test_bug_base ("https://phabricator.endlessm.com/");
+
+  g_test_add_func ("/unattended-config/empty", test_parse_empty);
+  g_test_add_func ("/unattended-config/malformed", test_parse_malformed);
+  g_test_add_func ("/unattended-config/noent", test_parse_noent);
+  g_test_add ("/unattended-config/unreadable", Fixture, NULL, fixture_set_up,
+              test_parse_unreadable, fixture_tear_down);
+
+  return g_test_run ();
+}
