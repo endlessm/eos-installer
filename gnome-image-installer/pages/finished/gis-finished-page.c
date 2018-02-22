56b48438d193395c14ef6b689a20fcddc002bb00
Will Thompson <will@willthompson.co.uk>
2018-02-22 16:49:59 +0000

    finished: convert to composite widget template

--- gnome-image-installer/pages/finished/gis-finished-page.c
+++ gnome-image-installer/pages/finished/gis-finished-page.c
@@ -46,13 +46,20 @@ struct _GisFinishedPagePrivate {
   gint led_state;
 
   GtkAccelGroup *accel_group;
+
+  GtkWidget *success_box;
+  GtkWidget *removelabel_usb;
+  GtkWidget *removelabel_dvd;
+  GtkButton *restart_button;
+
+  GtkWidget *error_box;
+  GtkLabel *error_heading_label;
+  GtkLabel *error_label;
+  GtkLabel *support_label;
 };
 typedef struct _GisFinishedPagePrivate GisFinishedPagePrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (GisFinishedPage, gis_finished_page, GIS_TYPE_PAGE);
-
-#define OBJ(type,name) ((type)gtk_builder_get_object(GIS_PAGE(page)->builder,(name)))
-#define WID(name) OBJ(GtkWidget*,name)
 
 /* See endlessm/gnome-shell/js/gdm/authPrompt.js */
 #define CUSTOMER_SUPPORT_FILENAME "vendor-customer-support.ini"
@@ -191,6 +198,8 @@ is_eosdvd (void)
 static void
 gis_finished_page_shown (GisPage *page)
 {
+  GisFinishedPage *self = GIS_FINISHED_PAGE (page);
+  GisFinishedPagePrivate *priv = gis_finished_page_get_instance_private (self);
   GError *error = gis_store_get_error();
 
   gis_driver_save_data (GIS_PAGE (page)->driver);
@@ -198,20 +207,18 @@ gis_finished_page_shown (GisPage *page)
   if (error != NULL)
     {
       GisAssistant *assistant = gis_driver_get_assistant (page->driver);
-      GtkLabel *error_heading_label = OBJ (GtkLabel*, "error_heading_label");
-      GtkLabel *error_label = OBJ (GtkLabel*, "error_label");
 
       if (error->domain == GIS_UNATTENDED_ERROR)
-        gtk_label_set_text (error_heading_label,
+        gtk_label_set_text (priv->error_heading_label,
                             _("Oops, something is wrong with your unattended installation configuration."));
       /* Otherwise, leave the default message (indicating a problem with the
        * image file). TODO: handle other domains that indicate problems
        * elsewhere.
        */
 
-      gtk_label_set_text (error_label, error->message);
-      gtk_widget_show (WID ("error_box"));
-      gtk_widget_hide (WID ("success_box"));
+      gtk_label_set_text (priv->error_label, error->message);
+      gtk_widget_show (priv->error_box);
+      gtk_widget_hide (priv->success_box);
       gis_assistant_locale_changed (assistant);
 
       if (gis_store_is_unattended())
@@ -223,8 +230,8 @@ gis_finished_page_shown (GisPage *page)
     {
       gboolean optical = is_eosdvd ();
 
-      gtk_widget_set_visible (WID ("removelabel_usb"), !optical);
-      gtk_widget_set_visible (WID ("removelabel_dvd"),  optical);
+      gtk_widget_set_visible (priv->removelabel_usb, !optical);
+      gtk_widget_set_visible (priv->removelabel_dvd,  optical);
     }
 }
 
@@ -314,11 +321,9 @@ gis_finished_page_constructed (GObject *object)
 
   G_OBJECT_CLASS (gis_finished_page_parent_class)->constructed (object);
 
-  gtk_container_add (GTK_CONTAINER (page), WID ("finished-page"));
-
   gis_page_set_complete (GIS_PAGE (page), TRUE);
 
-  g_signal_connect (OBJ (GtkButton *, "restart_button"), "clicked", G_CALLBACK(reboot_cb), page);
+  g_signal_connect (priv->restart_button, "clicked", G_CALLBACK (reboot_cb), page);
 
   /* Use Ctrl+U to write unattended config */
   priv->accel_group = gtk_accel_group_new ();
@@ -339,9 +344,17 @@ gis_finished_page_dispose (GObject *object)
   G_OBJECT_CLASS (gis_finished_page_parent_class)->dispose (object);
 }
 
+static GtkBuilder *
+gis_finished_page_get_builder (GisPage *page)
+{
+  return NULL;
+}
+
 static void
 gis_finished_page_locale_changed (GisPage *page)
 {
+  GisFinishedPage *self = GIS_FINISHED_PAGE (page);
+  GisFinishedPagePrivate *priv = gis_finished_page_get_instance_private (self);
   g_autofree gchar *support_email = NULL;
   g_autofree gchar *support_email_markup = NULL;
   g_autofree gchar *support_markup = NULL;
@@ -361,7 +374,7 @@ gis_finished_page_locale_changed (GisPage *page)
   /* Translators: the %s is the customer support email address */
   support_markup = g_strdup_printf (_("Please contact %s or join the <a href=\"https://community.endlessos.com/\">Endless Community</a> to troubleshoot."),
                                     support_email_markup);
-  gtk_label_set_markup (OBJ (GtkLabel *, "support_label"), support_markup);
+  gtk_label_set_markup (priv->support_label, support_markup);
 }
 
 static GtkAccelGroup *
@@ -379,7 +392,20 @@ gis_finished_page_class_init (GisFinishedPageClass *klass)
   GisPageClass *page_class = GIS_PAGE_CLASS (klass);
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
+  gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass), "/org/gnome/initial-setup/gis-finished-page.ui");
+
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, success_box);
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, removelabel_usb);
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, removelabel_dvd);
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, restart_button);
+
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, error_box);
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, error_heading_label);
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, error_label);
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisFinishedPage, support_label);
+
   page_class->page_id = PAGE_ID;
+  page_class->get_builder = gis_finished_page_get_builder;
   page_class->locale_changed = gis_finished_page_locale_changed;
   page_class->get_accel_group = gis_finished_page_get_accel_group;
   page_class->shown = gis_finished_page_shown;
@@ -388,9 +414,11 @@ gis_finished_page_class_init (GisFinishedPageClass *klass)
 }
 
 static void
-gis_finished_page_init (GisFinishedPage *page)
+gis_finished_page_init (GisFinishedPage *self)
 {
   g_resources_register (finished_get_resource ());
+
+  gtk_widget_init_template (GTK_WIDGET (self));
 }
 
 void
